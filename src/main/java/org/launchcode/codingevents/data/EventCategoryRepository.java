4344dd13d19105bcacf46522a01e83426f2625e5
unknown <nunezfrancisco400@gmail.com>
2022-11-09 21:08:52 -0500

    last

--- src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventCategoryRepository.java
@@ -1,9 +1,7 @@
 package org.launchcode.codingevents.data;
 
-import org.launchcode.codingevents.models.EventCategory;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventCategoryRepository extends CrudRepository<EventCategory,Integer> {
+public interface EventCategoryRepository {
 }
--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -3,7 +3,7 @@ package org.launchcode.codingevents.data;
 import org.launchcode.codingevents.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository {
 
     User findByUsername(String username);
 }
