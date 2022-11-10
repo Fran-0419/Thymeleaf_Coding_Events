c7e41b73e49a9e7f8596184c527c34563094d40d
unknown <nunezfrancisco400@gmail.com>
2022-11-09 21:26:15 -0500

    fix

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -1,7 +1,9 @@
 package org.launchcode.codingevents.data;
 
+import org.launchcode.codingevents.models.Event;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository {
+public interface EventRepository extends CrudRepository<Event,Integer> {
 }
--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -3,7 +3,7 @@ package org.launchcode.codingevents.data;
 import org.launchcode.codingevents.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User,Integer>{
 
     User findByUsername(String username);
 }
